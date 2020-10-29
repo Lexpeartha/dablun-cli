79b78244b567c8aaa94380fd4bd1791d4475ff6e
Lex <sevic.official@gmail.com>
2020-10-29 20:52:10 +0100

    Added better visuals for consoling out API data

--- bin/dablun-check.js
+++ bin/dablun-check.js
@@ -6,9 +6,11 @@ program
 	.command("price")
 	.description("Check price of coins")
 	.option("--coin <type>", "Add specific coin types in CSV format", null)
-	.option("--cur <currency>", "Change the currecny", "USD")
+	.option("--cur <currency>", "Change the currency", "USD")
 	.option("--top <listingNumber>",
 	"Show specific amount of cryptocurrencies that are in trend", null)
+	// TO DO: Implement feature below
+	//.option("--status <statusActivity>", "Specify whether you want some active or inactive currencies to be shown, all options are: active, inactive and dead", "active")
 	.action(cmd => checkPrice(cmd));
 
 program.parse(process.argv);
